2eb81103374897e86bcd15cc0f13e0ff15dc533e
Nejc Gustincic <nejcgustincic@gmail.com>
2016-12-29 12:57:30 +0100

    Dodana sporočila poslano/prejeto

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 import os
 import jinja2
 import webapp2
+from google.appengine.api import users
 
 from models import Task
+from models import Mail
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
 
@@ -29,30 +31,68 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+        user = users.get_current_user()
+        if user:
+            logout_url = users.create_login_url("/")
+            params= {"user":user, "logout_url":logout_url}
+            return self.render_template("hello.html",params=params)
+        else:
+            return self.redirect_to("login")
+
+class OsnovaHandler(BaseHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            logout_url = users.create_login_url("/")
+            params = {"user": user, "logout_url": logout_url}
+            return self.render_template("base.html",params=params)
+        else:
+            return self.redirect_to("login")
+
+
+
 
 class DodajHandler(BaseHandler):
     def get(self):
-        return self.render_template("dodaj_task.html")
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
+        if user:
+            params = {"user":user,"logout_url":logout_url}
+            return self.render_template("dodaj_task.html",params=params)
+        else:
+            return self.redirect_to("login")
     
     def post(self):
-        sporocilo = self.request.get("sporocilo")
-        task = Task(sporocilo=sporocilo)
-        task.put()
-        return self.redirect_to("seznam")
+        user = users.get_current_user()
+        if user:
+
+            ime = user.nickname()
+            sporocilo = self.request.get("sporocilo")
+            task = Task(sporocilo=sporocilo, avtor= ime)
+            task.put()
+            return self.redirect_to("seznam")
+        else:
+            return self.redirect_to("login")
 
 class SeznamHandler(BaseHandler):
     def get(self):
-        taski = Task.query(Task.je_izbrisan == False).fetch()
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
+        if user:
+            taski = Task.query(Task.je_izbrisan == False).fetch()
 
-        params = {"taski":taski}
+            params = {"taski":taski,"user":user,"logout_url":logout_url}
 
-        return self.render_template("seznam.html", params=params)
+            return self.render_template("seznam.html", params=params)
+        else:
+            return self.redirect_to("login")
 
 class PosameznoHandler(BaseHandler):
     def get(self,task_id):
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
         task = Task.get_by_id(int(task_id))
-        params = {"task":task}
+        params = {"task":task,"user":user,"logout_url":logout_url}
         return self.render_template("task.html", params=params)
     def post(self,task_id):
         task = Task.get_by_id(int(task_id))
@@ -66,19 +106,23 @@ class PosameznoHandler(BaseHandler):
 
 class EditHandler(BaseHandler):
     def get(self,task_id):
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
         task = Task.get_by_id(int(task_id))
-        params = {"task":task}
+        params = {"task":task,"user":user,"logout_url":logout_url}
         return self.render_template("uredi.html", params=params)
 
     def post(self,task_id):
         task = Task.get_by_id(int(task_id))
-        task.sporocilo = self.request.get
+        task.sporocilo = self.request.get("text_taska")
         task.put()
         return self.redirect_to("seznam")
 class DeleteHandler(BaseHandler):
     def get(self,task_id):
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
         task = Task.get_by_id(int(task_id))
-        params = {"task": task}
+        params = {"task": task,"user":user,"logout_url":logout_url}
         return self.render_template("izbrisi.html", params=params)
     def post(self,task_id):
         task = Task.get_by_id(int(task_id))
@@ -88,33 +132,105 @@ class DeleteHandler(BaseHandler):
 
 class OpravljeniHandler(BaseHandler):
     def get(self):
-        taski = Task.query().fetch()
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
+        if user:
+            taski = Task.query().fetch()
 
-        params = {"taski":taski}
+            params = {"taski":taski,"user":user,"logout_url":logout_url}
 
-        return self.render_template("opravljeno.html", params=params)
+            return self.render_template("opravljeno.html", params=params)
+        else:
+            return self.redirect_to("login")
 
 class IzbrisanoHandler(BaseHandler):
     def get(self):
-        task = Task.query(Task.je_izbrisan == True).fetch()
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
+        if user:
+            task = Task.query(Task.je_izbrisan == True).fetch()
+            params = {"task": task,"user":user,"logout_url":logout_url}
+            return self.render_template("izbrisano.html", params=params)
+        else:
+            return self.redirect_to("login")
+
+class ObnoviHandler(BaseHandler):
+    def get(self, task_id):
+        task = Task.get_by_id(int(task_id))
         params = {"task": task}
-        return self.render_template("izbrisano.html", params=params)
+        return self.render_template("obnovi.html", params=params)
     def post(self,task_id):
         task = Task.get_by_id(int(task_id))
         task.je_izbrisan = False
         task.put()
         return self.redirect_to("seznam")
 
+class TrajnoIzbrisi(BaseHandler):
+    def get(self, task_id):
+        task = Task.get_by_id(int(task_id))
+        params = {"task": task}
+        return self.render_template("trajno.html", params=params)
+    def post(self,task_id):
+        task = Task.get_by_id(int(task_id))
+        task.key.delete()
+        return self.redirect_to("izbrisano")
 
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        login_url = users.create_login_url("/")
+        params = {"login_url": login_url}
+        return self.render_template("login.html", params=params)
+class MailHandler(BaseHandler):
+    def get(self):
+        user = users.get_current_user()
+        logout_url = users.create_login_url("/")
+        if user:
+            params={"user":user,"logout_url":logout_url}
+            return self.render_template("poslji-sporocilo.html",params=params)
+        else:
+            return self.redirect_to("login")
+
+    def post(self):
+        user = str(users.get_current_user())
+        besedilo = self.request.get("sporocilo")
+        naslovnik = self.request.get("naslovnik")
+        sporocilo= Mail(besedilo=besedilo, avtor=user,naslovnik=naslovnik)
+        sporocilo.put()
+        return self.redirect_to("poslji")
+class PrejetoHandler(BaseHandler):
+    def get(self):
+        logout_url = users.create_login_url("/")
+        user = users.get_current_user()
+        mail= Mail.query(Mail.naslovnik == user.email()).fetch()
+        params = {"mail":mail,"user":user,"logout_url":logout_url}
+        return self.render_template("prejeta-sporocila.html",params=params)
+
+class PoslanoHandler(BaseHandler):
+    def get(self):
+        logout_url = users.create_login_url("/")
+        user = users.get_current_user()
+        mail= Mail.query(Mail.avtor == str(user)).fetch()
+        params = {"mail":mail,"user":user,"logout_url":logout_url}
+        return self.render_template("poslana-sporocila.html",params=params)
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="glavna_stran"),
+    webapp2.Route('/login', LoginHandler, name="login"),
+    webapp2.Route('/base', OsnovaHandler),
     webapp2.Route('/add_task', DodajHandler),
     webapp2.Route('/seznam', SeznamHandler, name="seznam"),
     webapp2.Route('/opravljeno',OpravljeniHandler),
     webapp2.Route('/seznam/<task_id:\d+>', PosameznoHandler),
     webapp2.Route('/seznam/<task_id:\d+>/edit', EditHandler),
     webapp2.Route('/seznam/<task_id:\d+>/delete', DeleteHandler),
-    webapp2.Route('/izbrisano', IzbrisanoHandler),
+    webapp2.Route('/izbrisano',IzbrisanoHandler, name ="izbrisano"),
+    webapp2.Route('/izbrisano/<task_id:\d+>/obnovi', ObnoviHandler),
+    webapp2.Route('/izbrisano/<task_id:\d+>/izbrisi', TrajnoIzbrisi),
+    webapp2.Route('/poslji-sporocilo',MailHandler, name= "poslji"),
+    webapp2.Route('/prejeto',PrejetoHandler),
+    webapp2.Route('/poslano',PoslanoHandler),
+
+
 ], debug=True)
--- models.py
+++ models.py
@@ -5,3 +5,9 @@ class Task(ndb.Model):
     dokoncan = ndb.BooleanProperty(default = False)
     cas_nastanka = ndb.DateTimeProperty(auto_now_add = True)
     je_izbrisan = ndb.BooleanProperty(default = False)
+    avtor = ndb.StringProperty()
+
+class Mail(ndb.Model):
+    besedilo = ndb.StringProperty()
+    avtor = ndb.StringProperty()
+    naslovnik = ndb.StringProperty()
